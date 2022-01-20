4234df7416d14949c03e2ea48bbb2f99cf97878d
timur <borodatimur@gmail.com>
2022-01-20 23:13:54 +0300

    feat: свои доступы - сервис провайдер

---
+++ app/Providers/PermissionServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Orchid\Platform\ItemPermission;
+use Orchid\Platform\Dashboard;
+
+class PermissionServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot(Dashboard $dashboard)
+    {
+        $permissions = ItemPermission::group('Main')
+            ->addPermission('analytics', 'Access to data analytics')
+            ->addPermission('monitor', 'Access to the system monitor');
+
+        $dashboard->registerPermissions($permissions);
+    }
+}
